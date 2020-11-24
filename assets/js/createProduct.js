7b1ad69e32823db7cc221047a15f642df4ab8c54
JohaB-V <johanabautista0@gmail.com>
2020-11-24 12:09:16 -0500

    Fix bugs

--- assets/js/createProduct.js
+++ assets/js/createProduct.js
@@ -13,7 +13,10 @@ const createProduct = (shirt) => {
           <img src="${shirt.src}" class="img-product">
           <span class="w3-tag w3-display-topleft">New</span>
           <span class="w3-tag w3-display-bottomright">OverSize T : S/M</span>
-          <div class="w3-display-middle w3-display-hover">
+          <div class="w3-tag w3-display-bottomleft w3-display-hover wp"> 
+          <a href="https://wa.me/message/EZ2UDNX4VXM4N1"><i class="fa fa-whatsapp"></i></a>
+  
+          
           </div>
         </div>
         <p class="title-product">${shirt.title}<br><b>$ ${shirt.price}</b></p>
